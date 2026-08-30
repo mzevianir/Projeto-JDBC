2e79341c646b2d5db4753e5f11dc5511581300af
Mateus <mateus_zevi@hotmail.com>
2026-08-30 12:38:11 -0300

    Adicionado Seller insert e findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,5 +16,24 @@ public class Program {
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        List<Seller> listAll = sellerDao.findAll();
+        for (Seller obj : listAll) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Gaby", "gaby@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido! Novo id = " + newSeller.getId());
+
     }
 }
