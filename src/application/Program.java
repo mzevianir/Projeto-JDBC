093a7d3c1e0185b2c57fca83667c8ed678060518
Mateus <mateus_zevi@hotmail.com>
2026-08-31 17:47:01 -0300

    Adicionado o DAO do Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,45 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        // System.out.println("=== TEST 1: seller findById ===");
-        // Seller seller = sellerDao.findById(3);
-        // System.out.println(seller);
+        System.out.println("=== TEST 1: seller findById ===");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
-        // System.out.println("\n=== TEST 2: seller findByDepartment ===");
-        // Department department = new Department(2, null);
-        // List<Seller> list = sellerDao.findByDepartment(department);
-        // for (Seller obj : list) {
-        // System.out.println(obj);
-        // }
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
-        // System.out.println("\n=== TEST 3: seller findAll ===");
-        // List<Seller> listAll = sellerDao.findAll();
-        // for (Seller obj : listAll) {
-        // System.out.println(obj);
-        // }
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        List<Seller> listAll = sellerDao.findAll();
+        for (Seller obj : listAll) {
+            System.out.println(obj);
+        }
 
-        // System.out.println("\n=== TEST 4: seller insert ===");
-        // Seller newSeller = new Seller(null, "Gaby", "gaby@gmail.com", new Date(),
-        // 4000.00, department);
-        // sellerDao.insert(newSeller);
-        // System.out.println("Inserido! Novo id = " + newSeller.getId());
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Gaby", "gaby@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido! Novo id = " + newSeller.getId());
 
-        // System.out.println("\n=== TEST 5: seller update ===");
-        // seller = sellerDao.findById(15);
-        // seller.setName("Atualizado em tempo real");
-        // sellerDao.update(seller);
-        // System.out.println("Atualização completa!");
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(15);
+        seller.setName("Atualizado em tempo real");
+        sellerDao.update(seller);
+        System.out.println("Atualização completa!");
 
         System.out.println("\n=== TEST 6: seller delete ===");
         sellerDao.deleteById(15);
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -5,9 +5,9 @@ import java.util.List;
 import model.entities.Department;
 
 public interface DepartmentDao {
-    void insert(Department obj);
+    void insert(Department dep);
 
-    void update(Department obj);
+    void update(Department dep);
 
     void deleteById(Integer id);
 
